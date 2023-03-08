1f5b487bd02331cd2ccebfb7f95ec87e66fec2a7
s403o <eslam.adel.me@gmail.com>
2023-03-08 18:30:50 +0200

    reg passed

--- demo/src/test/java/anonfile_reg/registration_Test.java
+++ demo/src/test/java/anonfile_reg/registration_Test.java
@@ -1,7 +1,9 @@
 package anonfile_reg;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -19,7 +21,10 @@ public class registration_Test {
         s403o.findElement(By.xpath("//*[@id='site-wrapper']/div[4]/div[2]/form/div[1]/input")).sendKeys("testing@s403o"); // Username
         s403o.findElement(By.xpath("//*[@id='site-wrapper']/div[4]/div[2]/form/div[2]/input")).sendKeys("testing@s403o");; // Password
         s403o.findElement(By.xpath("//*[@id='site-wrapper']/div[4]/div[2]/form/div[3]/input")).sendKeys("testing@s403o");; // confirm Password
-        s403o.findElement(By.xpath("//*[@id='site-wrapper']/div[4]/div[2]/form/input[2]")).click(); // Click Create Account
+        WebElement create_btn = s403o.findElement(By.xpath("//*[@id='site-wrapper']/div[4]/div[2]/form/input[2]")); // looking for Create Account button
+        JavascriptExecutor java = (JavascriptExecutor)s403o; // casting the Jsexe to be a Webdriver
+        java.executeScript("scroll(0, 250)");
+        create_btn.click();
 
 
     }
