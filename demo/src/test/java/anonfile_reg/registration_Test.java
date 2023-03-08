e0e16b5c620a4b73cf62be1a8719aa44a53aff98
s403o <eslam.adel.me@gmail.com>
2023-03-08 17:48:10 +0200

    open our target in browser

---
+++ demo/src/test/java/anonfile_reg/registration_Test.java
@@ -0,0 +1,25 @@
+package anonfile_reg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * TestNG for registration at anonfile
+ */
+public class registration_Test {
+    WebDriver s403o; // interface
+    @Test
+    public void registration_Test_Case() {
+
+    }
+
+    @BeforeClass
+    public void open_browser() {
+        System.setProperty("webdriver.chrome.driver",
+                "/usr/bin/chromedriver");
+        s403o = new ChromeDriver(); // implment interface
+        s403o.get("https://anonfiles.com/");
+    }
+}
